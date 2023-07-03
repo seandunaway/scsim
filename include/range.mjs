e366c161ec10ac6605166c0febc10d316d08bbdf
seandunaway <git@seandunaway.com>
2023-07-03 12:54:37 -0700

    add diff_days

--- include/range.mjs
+++ include/range.mjs
@@ -20,5 +20,6 @@ export function debug() {
 export function post() {
     let low_datetime = new Date(low.t)
     let high_datetime = new Date(high.t)
-    return {low_datetime, low, high_datetime, high}
+    let diff_days = (high.t - low.t) / 86_400_000
+    return {low_datetime, low, high_datetime, high, diff_days}
 }
