619107fd9296729a08640f7595e2625dbf7b9a6f
seandunaway <git@seandunaway.com>
2023-07-03 18:27:45 -0700

    add 1r 100p 2017

---
+++ include/t_1r_100p_2017.mjs
@@ -0,0 +1,26 @@
+import * as trade from '../trade.mjs'
+
+export let enabled = true
+export let name = 'trade 1r, 100p targets, 2017 only'
+
+let state = trade.new_state()
+
+export function pre() {
+}
+
+export function tick(object) {
+    trade.trade_targets(state, object, 100, 100)
+
+    // enter
+    if (object.t < new Date('2017').getTime() || object.t > new Date('2018').getTime()) return
+    if (object.c % 1 !== 0) return  // every 1p
+    if (state.trades.includes(object.c)) return   // no duplicates
+    state.trades.push(object.c)
+}
+
+export function debug() {
+}
+
+export function post() {
+    return trade.trade_summary(state)
+}
