e063ebf1a2721389263e16d856580fe399ad0e7d
seandunaway <git@seandunaway.com>
2023-07-04 07:29:23 -0700

    simplified resolved state

--- trade.mjs
+++ trade.mjs
@@ -3,8 +3,6 @@ export function new_state(overrides) {
         trades: [],
         up: 0,
         down: 0,
-        resolved: 0,
-        unresolved: 0,
         ...overrides,
     }
     return state
@@ -14,18 +12,17 @@ export function trade_targets(state, object, up_target = 5, down_target = 5) {
     for (let i = 0; i < state.trades.length; i++) {
         if (state.trades[i] + up_target <= object.c) {
             state.up++
-            state.resolved++
             state.trades.splice(i, 1)
         }
         if (state.trades[i] - down_target >= object.c) {
             state.down++
-            state.resolved++
             state.trades.splice(i, 1)
         }
     }
 }
 
 export function trade_summary(state) {
+    state.resolved = state.up + state.down
     state.unresolved = state.trades.length
     state.total_trades = state.resolved + state.unresolved
     state.percent = state.up / state.resolved
